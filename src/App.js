b6ced54f46094cb88695af7b568e6de2a66da56c
Timothy Shee <timothy_shee@hotmail.com>
2020-11-24 17:52:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,20 +12,19 @@ import BrowsePage from './pages/Browse/BrowsePage';
 import SigninPage from './pages/SigninPage';
 import RegisterPage from './pages/RegisterPage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
-import ProfileDashboard from './pages/Dashboard/ProfileDashboard'
-import HistoryDashboard from './pages/Dashboard/HistoryDashboard'
-import SocialDashboard from './pages/Dashboard/SocialDashboard';
+import ProfileDashboard from './pages/ProfileDashboard'
+import HistoryDashboard from './pages/HistoryDashboard'
+import SocialDashboard from './pages/SocialDashboard';
+import HomeDashboard from './pages/HomeDashboard'
 // Sidebar icons
 import HomeIcon from '@material-ui/icons/Home'
 import PageviewIcon from '@material-ui/icons/Pageview';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import HistoryIcon from '@material-ui/icons/History';
 import PeopleIcon from '@material-ui/icons/People';
-// super hack
+import DashboardIcon from '@material-ui/icons/Dashboard';
+// get local from root
 import logo from './TastePerfect-Logo.png'
-
-// import HomeDashboard from './pages/Dashboard/HomeDashboard'
-// import DashboardIcon from '@material-ui/icons/Dashboard';
 
 function App(props) {
   // custom hook to call i18n changeLanguage function
