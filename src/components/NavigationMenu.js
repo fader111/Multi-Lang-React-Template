5431a1cdf15e054f22a1b305288323c6f006eaee
Timothy Shee <timothy_shee@hotmail.com>
2020-11-24 18:03:42 -0500

    Update NavigationMenu.js

--- src/components/NavigationMenu.js
+++ src/components/NavigationMenu.js
@@ -199,9 +199,6 @@ export default function PersistentDrawerLeft(props) {
               aria-label={"back to home image"}
               color="inherit"
             >
-              <Typography variant="button" display="block" gutterBottom>
-                {""}
-              </Typography>
               <img src={logo} className={classes.logo} alt="logo" />
             </Button>
           </CustomLink>
